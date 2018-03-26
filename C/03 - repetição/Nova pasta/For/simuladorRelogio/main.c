b830d26a6c88bf2b91ab26370c503620846ea6aa
Gelton Cruz <Gelton@midiaplural.com.br>
2018-03-26 17:37:45 -0300

    For exercicio

---
+++ C/03 - repetição/Nova pasta/For/simuladorRelogio/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int seg,min;
+    printf("O valor de minuto eh: %d\n",min);
+    for(min = 0; min < 2 ; min++){
+        for(seg = 0; seg < 60 ; seg++)
+        {
+          printf("%d seg. \n",seg);
+        }
+
+    }
+    printf("\nO valor de minuto eh: %d",min);
+    return 0;
+}
+
+1
+2,  3
+4,  5,  6
+7 , 8,  9,  10
+11, 12, 13, 14, 15
+
+1,1,2,3,5,8,13,21,34,55,89
+
+12 22 32
+88 78 68
+44 24 14
+
+
+
+
+
+
+
+
+
